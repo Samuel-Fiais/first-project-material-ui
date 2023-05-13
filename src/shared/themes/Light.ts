fda8100edff4969bf8371156d8285babbc8c9f5b
Samuel Fiais <samuelfiais.dev@gmail.com>
2023-05-13 14:35:24 -0300

    [Add] Light theme and provider

---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material'
+import { cyan, purple } from '@mui/material/colors'
+
+export const LightTheme = createTheme({
+	palette: {
+		primary: {
+			main: purple[700],
+			dark: purple[800],
+			light: purple[500],
+			contrastText: '#FFF',
+		},
+		secondary: {
+			main: cyan[700],
+			dark: cyan[800],
+			light: cyan[500],
+			contrastText: '#FFF',
+		},
+		background: {
+			default: '#F7F6F3',
+			paper: '#FFF',
+		}
+	}
+})
